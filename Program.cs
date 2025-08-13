4a417475b7ae263e32cbd3469c12bcf1e90bde29
Hubert <blu3xray@gmail.com>
2025-08-13 16:03:50 +0200

    Working basics
    Simplified ASP.NET and Angular setup. Basic working structure

--- Program.cs
+++ Program.cs
@@ -1,3 +1,6 @@
+using System.Diagnostics;
+using System.Net.Http.Headers;
+
 namespace AIClient.Web;
 
 public class Program
@@ -5,30 +8,40 @@ public class Program
     public static void Main(string[] args)
     {
         var builder = WebApplication.CreateBuilder(args);
+        
+        var myAllowSpecificOrigins = "_myAllowSpecificOrigins";
+        builder.Services.AddCors(options =>
+        {
+            options.AddPolicy(name: myAllowSpecificOrigins,
+                policy =>
+                {
+                    policy.WithOrigins("http://localhost:4200")
+                        .AllowAnyHeader()
+                        .AllowAnyMethod();
+                });
+        });
 
-        // Add services to the container.
-        builder.Services.AddControllersWithViews();
+        builder.Services.AddControllers();
+        builder.Services.AddEndpointsApiExplorer();
+        builder.Services.AddSwaggerGen();
+
+// ...
 
         var app = builder.Build();
-
-        // Configure the HTTP request pipeline.
-        if (!app.Environment.IsDevelopment())
+        
+        if (app.Environment.IsDevelopment())
         {
-            app.UseExceptionHandler("/Home/Error");
-            // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
-            app.UseHsts();
+            app.UseSwagger();
+            app.UseSwaggerUI();
         }
 
         app.UseHttpsRedirection();
-        app.UseStaticFiles();
-
-        app.UseRouting();
+        
+        app.UseCors(myAllowSpecificOrigins);
 
         app.UseAuthorization();
-
-        app.MapControllerRoute(
-            name: "default",
-            pattern: "{controller=Home}/{action=Index}/{id?}");
+        
+        app.MapControllers();
 
         app.Run();
     }
